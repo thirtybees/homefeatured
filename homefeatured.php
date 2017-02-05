350106c673c4ca23cc2807a1fc791eaee61b2d80
Michael Dekker <git@michaeldekker.nl>
2017-02-05 07:43:27 +0100

    Update homefeatured.php

--- homefeatured.php
+++ homefeatured.php
@@ -24,7 +24,7 @@
 *  International Registered Trademark & Property of PrestaShop SA
 */
 
-if (!defined('_PS_VERSION_'))
+if (!defined('_TB_VERSION_'))
 	exit;
 
 class HomeFeatured extends Module
@@ -35,8 +35,8 @@ class HomeFeatured extends Module
 	{
 		$this->name = 'homefeatured';
 		$this->tab = 'front_office_features';
-		$this->version = '1.8.1';
-		$this->author = 'PrestaShop';
+		$this->version = '2.0.0';
+		$this->author = 'thirty bees';
 		$this->need_instance = 0;
 
 		$this->bootstrap = true;
